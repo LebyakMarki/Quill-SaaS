60a87119ba4419cdd2cc6b5fd18e20cfdf537e7d
LebyakMarki <lebyak.m@gmail.com>
2023-11-22 20:14:19 +0100

    Add dashboard

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,6 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import {
+  privateProcedure,
   publicProcedure,
   router,
 } from './trpc'
@@ -32,7 +33,16 @@ export const appRouter = router({
     }
 
     return { success: true }
-  })
+  }),
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    })
+  }),
 })
 
 export type AppRouter = typeof appRouter
